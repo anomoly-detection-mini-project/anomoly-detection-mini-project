cea02dc2503d30e48e875690e779fb637ff51e21
Dev Gurung <dev.gurung141@gmail.com>
2023-02-23 10:02:11 -0600

    added docstring

--- wrangle.py
+++ wrangle.py
@@ -11,7 +11,11 @@ warnings.filterwarnings("ignore")
 pd.set_option('display.max_rows', None)
 
 
-def get_max_traffic(df):
+def get_program_cohort_max_traffic(df):
+    
+    '''takes a dataframe
+    return a dataframe with maximum traffic visit by program and cohort
+    '''
     
     df = df  [~((df.path.str.startswith('/'))|(df.path.str.startswith('appendix')) | (df.path.str.startswith('users'))|(df.path.str.startswith('warmup')))]
     
@@ -26,4 +30,34 @@ def get_max_traffic(df):
     
     df = pd.merge(df, df_1, how='inner', on=['program_id','name','proba_status'])
     
+    return df
+
+
+def get_program_max_traffic(df):
+    
+    '''takes a dataframe
+    return a dataframe with maximum traffic visit by program
+    '''
+    
+    df = df  [~((df.path.str.startswith('/'))|(df.path.str.startswith('appendix')) | (df.path.str.startswith('users'))|(df.path.str.startswith('warmup')))]
+    
+    df = (df.groupby(['program_id'])
+            .path.value_counts(normalize=True)
+            .rename('proba_status')
+            .reset_index())
+    
+    df_1 = pd.DataFrame(df.groupby(['program_id']).proba_status.max()).reset_index()
+    
+    df = pd.merge(df, df_1, how='inner', on=['program_id','proba_status'])
+    
+    return df
+
+def min_traffic(df):
+    
+    '''takes a dataframe
+    return a dataframe with minimum traffic visit
+    '''
+    
+    df = df  [~((df.path.str.startswith('/'))|(df.path.str.startswith('appendix')) | (df.path.str.startswith('users'))|(df.path.str.startswith('warmup')))]
+    df = pd.DataFrame(df.path.value_counts().rename('count'))
     return df
