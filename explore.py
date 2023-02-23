fe6d9c40f356d5930afe012c2d1c98160704ff58
Andy Jensen <andrew.j.jensen91@gmail.com>
2023-02-23 11:11:45 -0600

    update

--- explore.py
+++ explore.py
@@ -11,26 +11,7 @@ warnings.filterwarnings("ignore")
 pd.set_option('display.max_rows', None)
 
 
-def get_program_cohort_max_traffic(df):
-    
-    '''takes a dataframe
-    return a dataframe with maximum traffic visit by program and cohort
-    '''
-    
-    df = df  [~((df.path.str.startswith('/'))|(df.path.str.startswith('appendix')) | (df.path.str.startswith('users'))|(df.path.str.startswith('warmup')))]
-    
-    df = df[df.name != 'Staff']
-    
-    df = (df.groupby(['program_id','name'])
-            .path.value_counts(normalize=True)
-            .rename('proba_status')
-            .reset_index())
-    
-    df_1 = pd.DataFrame(df.groupby(['program_id','name']).proba_status.max()).reset_index()
-    
-    df = pd.merge(df, df_1, how='inner', on=['program_id','name','proba_status'])
-    
-    return df
+
 
 
 def get_program_max_traffic(df):
@@ -40,7 +21,7 @@ def get_program_max_traffic(df):
     '''
     
     df = df  [~((df.path.str.startswith('/'))|(df.path.str.startswith('appendix')) | (df.path.str.startswith('users'))|(df.path.str.startswith('warmup')))]
-    
+    df = df[df.name != 'Staff']
     df = (df.groupby(['program_id'])
             .path.value_counts(normalize=True)
             .rename('proba_status')
